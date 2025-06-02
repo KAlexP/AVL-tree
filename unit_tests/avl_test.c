18a3fd6a16fe762f90ee88c3cc01e5a1368bc373
Kelson Petersen <kelsonpetersen@icloud.com>
2025-06-02 13:54:56 -0600

    adds string type to avl_test

--- unit_tests/avl_test.c
+++ unit_tests/avl_test.c
@@ -1,36 +1,24 @@
 #include "./../src/AVLtree.h"
 #include <assert.h>
+#include <string.h>
 
 int main(void) {
   int nums[] = {12, 56, 345, 46, 66, 99, 1, 4, 5, 6, 14, 47, 76};
+  char *str[] = {"cat", "dog", "donkey", "turkey", "bull", "aggie", "chicken"};
   node *root = AVLtree();
-  void *data = malloc(sizeof(int));
+  void *data;
   node *temp;
   int x;
-  /*
-
-  *(int *)data = 22;
-  printf("printing NULL tree\n");
-  print_tree(root, print_int, stdout);
-  temp = make_node(data);
-  printf("\ninserting 22...\n\n");
-  insert(&root, temp, compare_int);
-  print_tree(root, print_int, stdout);
-
-  printf("\ninserting 36...\n\n");
-  data = malloc(sizeof(int));
-  *(int *)data = 36;
-  temp = make_node(data);
-  insert(&root, temp, compare_int);
-  print_tree(root, print_int, stdout);
-*/
+  node *str_root = AVLtree();
+  void *str_data;
+  node *str_temp;
   for (x = 0; x < 13; ++x) {
-    printf("inserting %d...\n", nums[x]);
+    printf("\ninserting %d...\n", nums[x]);
     data = malloc(sizeof(int));
     *(int *)data = nums[x];
     temp = make_node(data);
     insert(&root, temp, compare_int);
-    printf("insert complete...\n");
+    printf("insert complete...\n\n");
     print_tree(root, print_int, stdout);
   }
   data = malloc(sizeof(int));
@@ -40,5 +28,15 @@ int main(void) {
   printf("inserting last node again\n");
   insert(&root, temp, compare_int);
   print_tree(root, print_int2, stdout);
+  printf("\n\n\n\n");
+  for (x = 0; x < 6; ++x) {
+    printf("\ninserting %s...\n", str[x]);
+    str_data = malloc(sizeof(char) * (strlen(str[x]) + 1));
+    strcpy(str_data, str[x]);
+    str_temp = make_node(str_data);
+    insert(&str_root, str_temp, compare_string);
+    printf("insert complete...\n\n");
+    print_tree(str_root, print_string, stdout);
+  }
   return 0;
 }
